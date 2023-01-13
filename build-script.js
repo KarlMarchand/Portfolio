60fb2561a76e9d41974be36339830159f02c5678
Karlipouette <karl-marchand@hotmail.com>
2023-01-13 02:29:13 -0500

    Working on project component

--- build-script.js
+++ build-script.js
@@ -41,15 +41,12 @@ const combineProjects = async (buildMode) => {
 				.map((image) => path.join(PROJECTS_DIST, projectDir, image));
 
 			// Look for a video in the project's directory
-			try {
-				const path = path.join(PROJECTS_ROOT, projectDir, "video.webm");
-				fs.accessSync(path);
-				// file exists
-				projectData.video = path.join(PROJECTS_DIST, projectDir, "video.webm");
-			} catch (e) {
-				// file does not exists, add an empty string
-				projectData.video = "";
-			}
+			const videos = fs
+				.readdirSync(path.join(PROJECTS_ROOT, projectDir))
+				.filter((file) => file.endsWith(".webm") || file.endsWith(".mp4"))
+				.map((video) => path.join(PROJECTS_DIST, projectDir, video));
+
+			projectData.video = videos.shift() ?? "";
 
 			// Add the project data to the combined data object
 			return projectData;
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,6 +1,7 @@
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, ListGroup } from "react-bootstrap";
 import ProjectData from "../types/ProjectData";
 import ProjectHeader from "../components/ProjectHeader";
+import { BsFillTagFill } from "react-icons/bs";
 
 const Project: React.FC<{ project: ProjectData }> = ({ project }) => {
 	return (
@@ -13,20 +14,65 @@ const Project: React.FC<{ project: ProjectData }> = ({ project }) => {
 				<Col md={{ span: 10, offset: 1 }} className="px-3">
 					<Row>
 						<Col lg={8}>
-							<div className="ratio ratio-16x9">
-								{project.video.length > 0 && <video src={project.video} controls />}
-								{!(project.video.length > 0) && (
+							<div>
+								{project.video.length > 0 && (
+									<video
+										src={project.video}
+										controls
+										style={{ maxWidth: "100%", maxHeight: "100%" }}
+									/>
+								)}
+								{project.video.length === 0 && (
 									<Image fluid src={project.thumbnailUrl} alt="Project Thumbnail" />
 								)}
 							</div>
 						</Col>
 						<Col lg={4}>
-							<p>{project.overview}</p>
+							<h3>Overview</h3>
+							<p className="mt-3">{project.overview}</p>
+							<ListGroup variant="flush" className="bg-dark my-5">
+								{project.tags.map((tag) => {
+									return (
+										<ListGroup.Item
+											className="bg-dark text-light mb-2"
+											style={{ borderColor: "var(--bs-light)" }}
+										>
+											<BsFillTagFill fill="var(--bs-primary)" />
+											<span className="ms-2">{tag}</span>
+										</ListGroup.Item>
+									);
+								})}
+							</ListGroup>
 						</Col>
 					</Row>
 				</Col>
 			</Row>
-			<Row>{/* render other project details here */}</Row>
+			<Row>
+				<Col md={{ span: 10, offset: 1 }} className="px-3 pt-4">
+					{/* render other project details here */}
+					<section className="mb-5 mt-2">
+						<h2>Description</h2>
+						<p>{project.description}</p>
+					</section>
+					<section className="mb-5">
+						<h2>Role</h2>
+						<p>{project.role}</p>
+					</section>
+					<section>
+						<h2>Technologies</h2>
+						<ListGroup variant="flush">
+							{Object.entries(project.technologies).map(([key, value]) => {
+								return (
+									<ListGroup.Item className="ms-3 mb-2 bg-dark text-light">
+										<h3>{key.charAt(0).toUpperCase() + key.slice(1)}</h3>
+										<p>{value}</p>
+									</ListGroup.Item>
+								);
+							})}
+						</ListGroup>
+					</section>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
